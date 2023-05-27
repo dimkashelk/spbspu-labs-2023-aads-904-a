0e3b1348812a0086de7f364065d78233429c077f
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2023-05-27 18:51:25 +0300

    check parameters

--- shelkovnikov.dmitrii/S5/main.cpp
+++ shelkovnikov.dmitrii/S5/main.cpp
@@ -1,4 +1,13 @@
-int main()
+#include <iostream>
+#include <fstream>
+int main(int argc, char *argv[])
 {
+  if (argc != 3)
+  {
+    std::cout << "Check parameters";
+    return 1;
+  }
+  std::ifstream in(argv[2]);
+
   return 0;
 }
