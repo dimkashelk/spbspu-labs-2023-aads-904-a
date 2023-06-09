684bf12f20528ade5852260f544fac1777c176d6
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2023-06-09 23:30:40 +0300

    add input of dict

--- shelkovnikov.dmitrii/S5/main.cpp
+++ shelkovnikov.dmitrii/S5/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include "twothreetree/twoThreeTree.h"
 int main(int argc, char *argv[])
 {
   if (argc != 3)
@@ -8,6 +9,23 @@ int main(int argc, char *argv[])
     return 1;
   }
   std::ifstream in(argv[2]);
-
+  if (!in)
+  {
+    std::cout << "Check file";
+    return 2;
+  }
+  using dict_type = dimkashelk::TwoThreeTree< int, std::string, std::less< > >;
+  dict_type data;
+  int key = 0;
+  std::string value;
+  while (in)
+  {
+    in >> key >> value;
+    if (!in)
+    {
+      break;
+    }
+    data.insert(key, value);
+  }
   return 0;
 }
